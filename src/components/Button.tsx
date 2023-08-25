93984958c8208a1a6f40a25591f07ffc7fdb2646
ukashu <lukaszfilipiuk123@gmail.com>
2023-08-25 20:37:40 +0200

    added shadow to window buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 export default function Button(props: Props) {
   return (
-    <button className="flex aspect-square h-full flex-col items-center border-b-2 border-l-4 border-r-2 border-t-4 border-b-zinc-500 border-l-zinc-200 border-r-zinc-500 border-t-zinc-200">
+    <button className="flex aspect-square h-full flex-col items-center border-b-2 border-l-4 border-r-2 border-t-4 border-b-zinc-500 border-l-zinc-200 border-r-zinc-500 border-t-zinc-200 shadow-[2px_2px_0_rgba(0,0,0,1)]">
       <p className="text-black">{props.icon}</p>
     </button>
   )
