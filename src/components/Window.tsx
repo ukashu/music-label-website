61029dbf71360af755d5c4c73d6f2d6369ceb148
ukashu <lukaszfilipiuk123@gmail.com>
2023-08-28 08:27:07 +0200

    window resizes with its content

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -9,7 +9,7 @@ type Props = {
 export default function Window(props: Props) {
   return (
     <div
-      className="absolute bottom-0 left-0 right-0 top-0 m-auto flex h-[200px] w-[200px] flex-col border-b-2 border-l-4 border-r-2 border-t-4 border-b-zinc-500 border-l-zinc-200 border-r-zinc-500 border-t-zinc-200"
+      className="absolute bottom-0 left-0 right-0 top-0 m-auto flex h-max w-max flex-col border-b-2 border-l-4 border-r-2 border-t-4 border-b-zinc-500 border-l-zinc-200 border-r-zinc-500 border-t-zinc-200"
       id="window"
       ref={props.headerReference}>
       <header
@@ -20,7 +20,9 @@ export default function Window(props: Props) {
         <Button icon="?" />
         <Button icon="x" />
       </header>
-      <main className="flex-grow bg-zinc-950">CONTENT</main>
+      <main className="flex-grow bg-zinc-950">
+        <div className=" h-[500px] w-[500px]">CONTENT</div>
+      </main>
     </div>
   )
 }
