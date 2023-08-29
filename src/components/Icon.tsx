5611627752326e09dc11cc206f524c305d9bdcdc
ukashu <lukaszfilipiuk123@gmail.com>
2023-08-29 17:56:43 +0200

    changed icon size

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -6,7 +6,7 @@ type Props = {
 export default function Icon(props: Props) {
   return (
     <div className=" flex flex-col items-center justify-between text-center">
-      <img src={props.icon} alt="Album icon" className=" blur-xs h-20 w-20" />
+      <img src={props.icon} alt="Album icon" className=" blur-xs h-16 w-16" />
       <p>{props.name}</p>
     </div>
   )
