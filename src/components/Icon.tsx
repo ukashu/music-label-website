bf7579bb91a5494f0217d68fe6506edc05c8f1d8
ukashu <lukaszfilipiuk123@gmail.com>
2023-08-31 12:43:12 +0200

    added terminal-like loading state

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -7,7 +7,7 @@ type Props = {
 export default function Icon(props: Props) {
   return (
     <div onClick={props.openFunction} className=" flex flex-col items-center justify-between text-center text-2xl">
-      <img src={props.icon} alt="Album icon" className=" h-16 w-16" />
+      <img src={props.icon} alt="Album icon" className=" h-16 w-16" id="icon" />
       <p>{props.name}</p>
     </div>
   )
