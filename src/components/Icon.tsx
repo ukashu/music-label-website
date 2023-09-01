d7ab1e399292c16f6291d5ad5c4a907770fdecae
ukashu <lukaszfilipiuk123@gmail.com>
2023-09-01 20:18:14 +0200

    added hover effect to icons and changed cursor to crosshair

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -6,7 +6,9 @@ type Props = {
 
 export default function Icon(props: Props) {
   return (
-    <div onClick={props.openFunction} className=" flex flex-col items-center justify-between text-center text-2xl">
+    <div
+      onClick={props.openFunction}
+      className=" flex flex-col items-center justify-between text-center text-2xl hover:animate-pulse hover:cursor-pointer">
       <img src={props.icon} alt="Album icon" className=" h-16 w-16" id="icon" />
       <p>{props.name}</p>
     </div>
